f386e9966174b7d1cda0f76dfb46fb5afa74fb8e
gooeye <32929088+gooeye@users.noreply.github.com>
2019-05-09 23:12:48 +0800

    Update oldtv.js
    Fixed lose-screen trigger on menu bug with event capturing rather than event bubbling
    Also possibly fixed another trigger error where clicking fast give extra points (not sure if this was even an issue though)

--- oldtv.js
+++ oldtv.js
@@ -10,18 +10,19 @@ var m=2;
 var score=0;
 document.getElementById("start").onclick=function() {
     document.getElementById("start").style.display="none";
+    document.getElementById("colour").display="inline";
     var c=Math.floor((Math.random()*colours.length));
     do {
         var d=Math.floor((Math.random()*colours.length));
     } while (d==c)
     if(Math.random()<0.5) {
-        m=1;
         document.getElementById("colour").style.color=colours[c][1];
         document.getElementById("colour").innerHTML=colours[c][0];
+        m=1;
     } else {
-        m=0;
         document.getElementById("colour").style.color=colours[d][1];
         document.getElementById("colour").innerHTML=colours[c][0];
+        m=0;
     }
 }
 document.addEventListener('contextmenu', function(e) {
@@ -43,13 +44,14 @@ document.addEventListener('contextmenu', function(e) {
             document.getElementById("colour").innerHTML=colours[c][0];
         }
     } else if(m==1) {
-        alert("you lose! your score is "+score);
+        document.getElementById("colour").display="none";
         m=2;
+        alert("you lose! your score is "+score);
         score=0;
         document.getElementById("start").style.display="block";
         document.getElementById("colour").innerHTML="";
     }
-}, false);
+}, true);
 document.addEventListener("click", function() {
     if(m==2) {
     } else if(m==1) {
@@ -68,10 +70,11 @@ document.addEventListener("click", function() {
             document.getElementById("colour").innerHTML=colours[c][0];
         }
     } else if(m==0) {
-        alert("you lose! your score is "+score);
+        document.getElementById("colour").display="none";
         m=2;
+        alert("you lose! your score is "+score);
         score=0;
         document.getElementById("start").style.display="block";
         document.getElementById("colour").innerHTML="";
     }
-});
+}, true);
