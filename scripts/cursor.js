d210cc271b445599a0218033f662d928289b27f0
gooeye <uniconpatio@gmail.com>
2023-11-17 01:30:01 +0800

    added option to change cursor size

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -6,6 +6,9 @@ cursor.style.pointerEvents = "none";
 parent.appendChild(cursor);
 document.body.appendChild(parent);
 var lineSize = 5;
+document.getElementById("cursorSize").innerText = lineSize;
+const minLineSize = 1;
+const maxLineSize = 10
 function moveCursor(e) {
     let x, y;
     x = e.clientX - lineSize;
@@ -16,9 +19,12 @@ function moveCursor(e) {
         cursor.classList.add("c1");
     } else if (el && el.closest(".card")) {
         cursor.classList.add("c2");
+    } else if (el && el.closest(".pin")) {
+        cursor.classList.add("faded");
     } else{
         cursor.classList.remove("c1");
         cursor.classList.remove("c2");
+        cursor.classList.remove("faded");
     }
     if (el && el.classList.contains("content")) {
         let rect = el.getBoundingClientRect();
@@ -28,12 +34,13 @@ function moveCursor(e) {
             cursor.style.height = rect.bottom - rect.top + 'px';
             cursor.style.left = rect.left - lineSize * 2 + 'px';
             cursor.style.top = rect.top + 'px';
+            parent.classList.remove("shadow");
         } else if (el.classList.contains("content-alt")) {
             cursor.setAttribute("data-text", el.alt);
-            cursor.style.width = lineSize * 20 + 'px';
-            cursor.style.height = lineSize * 8 + 'px';
-            cursor.style.left = rect.left + lineSize * 4 + 'px';
-            cursor.style.top = rect.top - lineSize * 8 + 'px';
+            cursor.style.width = '100px';
+            cursor.style.height = '40px';
+            cursor.style.left = rect.left + 20 + 'px';
+            cursor.style.top = rect.top - 40 + 'px';
             cursor.classList.add("alt");
             parent.classList.add("shadow");
         } else {
@@ -41,6 +48,8 @@ function moveCursor(e) {
             cursor.style.height = lineSize + 'px';
             cursor.style.left = rect.left + 'px';
             cursor.style.top = rect.bottom + 'px';
+            cursor.classList.remove("alt");
+            parent.classList.remove("shadow");
         }
     } else {
         cursor.style.width = lineSize * 2 + 'px';
@@ -60,10 +69,19 @@ document.addEventListener('mousemove', (e) => {
         return;
     }
     enableCall = false;
-    moveCursor(e);
     setTimeout(() => {
         enableCall = true;
         moveCursor(lastE);
     }, 50);
 });
 document.addEventListener('scroll', () => {moveCursor(lastE)});
+document.getElementById('decrement').addEventListener('click', () => {
+    if (lineSize > minLineSize) lineSize--;
+    document.getElementById("cursorSize").innerText = lineSize;
+    moveCursor(lastE);
+})
+document.getElementById('increment').addEventListener('click', () => {
+    if (lineSize < maxLineSize) lineSize++;
+    document.getElementById("cursorSize").innerText = lineSize;
+    moveCursor(lastE);
+})
